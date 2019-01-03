1e18815e86c1c728e51b0c7d4a4785252b4358d3
Wan Mohd Adzha <wmadzha11@hotmail.com>
2019-01-04 02:49:48 +0800

    Create CrashKey.cs

---
+++ CrashKey.cs
@@ -0,0 +1,33 @@
+
+
+namespace CrashReportLibrary
+{
+    public static class CrashKey
+    {
+        public static string GetKey(string Env , string Country)
+        {
+            if(Env == Constant.Env.BlueGreen)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.Dev)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.TestUI)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.Pilot)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.TestBE)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.Live && Country == Constant.Country.De)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.Live && Country == Constant.Country.My)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.Live && Country == Constant.Country.Ne)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.Live && Country == Constant.Country.Fr)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            if (Env == Constant.Env.Live && Country == Constant.Country.Sg)
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+            else
+                return "SampleKey-72b3-41be-b01a-2152d70448c7";
+        }
+    }
+}
