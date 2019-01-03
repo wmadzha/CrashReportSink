b93dfbf9b50421a106f5618be073ae9b801b294d
Wan Mohd Adzha <wmadzha11@hotmail.com>
2019-01-04 02:49:15 +0800

    Create Constant.cs

---
+++ Constant.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace CrashReportLibrary
+{
+    public  static class Constant
+    {
+        public static class Env
+        {
+            public const string Dev = "Dev";
+            public const string BlueGreen = "BlueGreen";
+            public const string Pilot = "Pilot";
+            public const string TestUI = "TestUI";
+            public const string TestBE = "TestBackEnd";
+            public const string Live = "Live";
+        }
+
+        public static class Country
+        {
+            public const string My = "Malaysia";
+            public const string Sg = "Singapore";
+            public const string De = "Germany";
+            public const string Ne = "Netherlands";
+            public const string Fr = "France";
+        }
+
+    }
+}
