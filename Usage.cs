676b71778da9fc89fb68c9a0671f7c71ea7034c9
Wan Mohd Adzha <wmadzha11@hotmail.com>
2019-01-04 14:48:11 +0800

    Update Usage.cs

--- Usage.cs
+++ Usage.cs
@@ -7,5 +7,18 @@ namespace ClientUsage
         {
             AppCenterStart.Start(Config.Env, Config.Country);
         }
+        public void Sample()
+        {
+            try
+            {
+                int Zeros = 0;
+                int temp = 1 / Zeros;
+            }
+            catch (System.Exception ex)
+            {
+                Report.SendReport(ex);
+                // Handle Exception
+            }
+        }
     }
 }
